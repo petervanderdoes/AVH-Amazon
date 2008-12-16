9c09c87879877291735333339ce6e2697e5e2d3d
Peter van der Does <peter@avirtualhome.com>
2008-12-16 12:40:08 -0500

    Trac Ticket #7 Since WordPress 2.7 there is an option to uninstall your plugin when a user decides to delete it.

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+// This is an include file, all normal WordPress functions will still work.
+// Because the plugin is already deactivated it won't regonize any class declarations.
+
+if ( ! defined ( 'ABSPATH' ) && ! defined ( 'WP_UNINSTALL_PLUGIN' ) ) exit ();
+if ( 'avh-amazon' == dirname ( $file ) ) {
+	delete_option ( 'avhamazon' );
+	delete_option ( 'widget_avhamazon_wishlist' );
+}
+
+?>
