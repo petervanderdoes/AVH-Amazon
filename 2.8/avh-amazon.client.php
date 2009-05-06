ed9baf7ece7b4180b1c8b3dfa99c7d7d7469d5f6
Peter van der Does <peter@avirtualhome.com>
2009-05-06 13:28:11 -0500

    Css Changes

--- 2.8/avh-amazon.client.php
+++ 2.8/avh-amazon.client.php
@@ -91,7 +91,7 @@ class AVHAmazonCore
 	 */
 	function __construct ()
 	{
-		$this->version = "2.4";
+		$this->version = "3.0-rc1";
 		$this->comment_begin = '<!-- AVH Amazon version ' . $this->version . ' Begin -->';
 		$this->comment_end = '<!-- AVH Amazon version ' . $this->version . ' End -->';
 
