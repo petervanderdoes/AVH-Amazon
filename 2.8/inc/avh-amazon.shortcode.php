5f60d71516929c0b20bacf67a8eb292ad713ca12
Peter van der Does <peter@avirtualhome.com>
2009-05-07 08:48:35 -0500

    Removed the line for ui-tabs

--- 2.8/inc/avh-amazon.shortcode.php
+++ 2.8/inc/avh-amazon.shortcode.php
@@ -44,7 +44,6 @@ class AVHAmazonShortcode extends AVHAmazonCore
 	{
 		if ( $GLOBALS['editing'] ) { //@todo Check if there's a better solution for this.
 			wp_enqueue_script( 'avhamazonmetabox', $this->info['install_url'] . '/inc/js/metabox.js', array ('jquery' ), $this->version );
-			//wp_enqueue_script( 'jquery-ui-tabs' );
 		}
 	}
 
