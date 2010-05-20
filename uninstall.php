02da6095b3db4bffbaa51530c49b85e50e3e300b
Peter van der Does <peter@avirtualhome.com>
2010-05-20 10:26:01 -0500

    Uninstall has to delete the uli pics database

--- uninstall.php
+++ uninstall.php
@@ -2,13 +2,13 @@
 // This is an include file, all normal WordPress functions will still work.
 // Because the plugin is already deactivated it won't regonize any class declarations.
 
-if ( ! defined ( 'ABSPATH' ) && ! defined ( 'WP_UNINSTALL_PLUGIN' ) ) exit ();
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 
-if ( 'avh-amazon' == dirname ( $file ) ) {
-	delete_option ( 'avhamazon' );
-	delete_option ( 'widget_avhamazon_wishlist' );
-	delete_option('avhamazon_cached_wishlist');
-	delete_option('avhamazon_cached_items');
+if ( 'avh-amazon' == dirname( $file ) ) {
+	delete_option( 'avhamazon' );
+	delete_option( 'widget_avhamazon_wishlist' );
+	delete_option( 'avhamazon_cached_wishlist' );
+	delete_option( 'avhamazon_cached_items' );
+	delete_option( 'avhamazon_uli_pics' );
 }
-
 ?>
